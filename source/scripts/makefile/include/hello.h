a622caba9f560bbbe0cbf1374d3c934d3cfd947e
zheng.dongxiong <zdx_97@163.com>
2022-03-27 18:02:52 +0800

    更新makefile添加install选项

---
+++ source/scripts/makefile/include/hello.h
@@ -0,0 +1,8 @@
+#ifndef __HELLO_H__
+#define __HELLO_H__
+
+int hello513();
+int hello794();
+
+
+#endif
--- source/scripts/makefile/tools/a.c
+++ source/scripts/makefile/tools/a.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <hello.h>
 
 
 int main (int argc, char *argv[])
